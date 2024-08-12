1877ad4b5c40205f6d3842ebd3e93adfcf6051c8
Kristina Rabatinova <kristina.rabatinova@gmail.com>
2024-08-12 16:57:18 +0200

    update: backround_LCI_database starting project with brightway

--- src/pyH2A/Plugins/Backround_LCI_Database_Plugin_Option1.py
+++ src/pyH2A/Plugins/Backround_LCI_Database_Plugin_Option1.py
@@ -4,11 +4,24 @@ import bw2analyzer as ba
 import bw2calc as bc
 import bw2data as bd
 import bw2io as bi
-#importing most important data science packages —› those are suggested by brightway getting started protocol —› so change it as you need in the future
+#importing most important data science packages —› those are suggested by brightway getting started protocol (https://learn.brightway.dev/en/latest/content/notebooks/BW25_for_beginners.html#section1)
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-#STARTING A PROJECT
-#selecting a project for bw
-bd.profect.set_current(name='<name_incomming>')
+import os
+#
+#WORK WITH A PROJECT
+#
+#creating a new project
+name = "testing_240812" #when you change the name here then the project is directly added to the list of the projects
+bd.projects.set_current(name)
+#listing all available projects and checking if "name" was added to the project list
+#print(bd.projects)
+#
+#DATABASES
+#setting up database
+
+#listing all available databases in the project
+#print(bd.databases)
+
 
