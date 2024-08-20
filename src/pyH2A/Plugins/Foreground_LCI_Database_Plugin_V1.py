8bbb439b0502b41e75bba542d4ef66e0849f49eb
Kristina Rabatinova <kristina.rabatinova@gmail.com>
2024-08-20 17:45:07 +0200

    update: adding the proper plugin structure

--- src/pyH2A/Plugins/Foreground_LCI_Database_Plugin_V1.py
+++ src/pyH2A/Plugins/Foreground_LCI_Database_Plugin_V1.py
@@ -1,190 +1,201 @@
-import json
+from pyH2A.Utilities.input_modification import insert, process_table, read_textfile
+import numpy as np
 import pprint as pp
-#listing of the ACTIVITIES
-activities = {
-    "activities": [
-        {
-            "name":"Pure water production",
-            "location": "GLO",
-            "reference product": "pure water",
-            "unit": "cubic meter",
-            "code": "pure_water_production"
-        },
-        {
-            "name": "Electrolysis of water",
-            "location": "GLO",
-            "reference product": "hydrogen",
-            "unit": "kilogram",
-            "code": "electrolysis_of_water" 
-        },
-        {
-            "name": "PV electricity production",
-            "location": "GLO/local", #not sure since the sunlight (one of the inputs) is dependent on the location
-            "reference product": "electricity",
-            "unit": "kilowatt",
-            "code": "pv_electricity_production" 
-        },
-        {
-            "name": "excessive power storage",
-            "location": "GLO/local", #same as above
-            "reference product": "electricity",
-            "unit": "kilowatt",
-            "code": "excessive_power_storage" 
-        }
-    ]
-}
-#listing of the EXCHANGES
-exchanges = {
-    "exchanges": [
-        #reverse osmosis
-        {
-            "input": "electricity",  # for reverse osmosis from PV
-            "amount": 1.0,  # !!!! specify the correct amount
-            "type": "technosphere",
-            "unit": "kilowatt",
-            "activity": "pure_water_production"
-        },
-        {
-            "input": "production_reverse_osmosis",
-            "amount": 1.0,
-            "type": "production",
-            "unit": "smt",  # !!!! specify the correct unit
-            "activity": "pure_water_production"
-        },
-        {
-            "input": "sea water",
-            "amount": 1.0,  # !!!! specify the correct amount
-            "type": "biosphere",
-            "unit": "cubic meter",
-            "activity": "pure_water_production"
-        },
-        {
-            "input": "pure water",
-            "amount": 1.0,  # !!!! specify the correct amount
-            "type": "production",
-            "unit": "cubic meter",
-            "activity": "pure_water_production"
-        },
-        {
-            "input": "brine",
-            "amount": 1.0,  # !!!! specify the correct amount
-            "type": "byproduct",
-            "unit": "kilogram",
-            "activity": "pure_water_production"
-        },
-        # water electrolysis
-        {
-            "input": "electricity",  # for water electrolysis from PV
-            "amount": 1.0,  # !!!! specify the correct amount
-            "type": "technosphere",
-            "unit": "kilowatt",
-            "activity": "electrolysis_of_water"
-        },
-        {
-            "input": "pure water",
-            "amount": 1.0,  # !!!! specify the correct amount
-            "type": "technosphere",
-            "unit": "cubic meter",
-            "activity": "electrolysis_of_water"
-        },
-        {
-            "input": "production_electrolyzer",
-            "amount": 1.0,  # if we need to replace this, then here it should change the amount
-            "type": "technosphere",
-            "unit": "kilowatt",
-            "activity": "electrolysis_of_water"
-        },
-        {
-            "input": "hydrogen",  # for water electrolysis from PV
-            "amount": 1.0,  # !!!! specify the correct amount
-            "type": "production",
-            "unit": "kilogram",
-            "activity": "electrolysis_of_water"
-        },
-        {
-            "input": "oxygen",  # for water electrolysis from PV
-            "amount": 1.0,  # !!!! specify the correct amount
-            "type": "byproduct",
-            "unit": "kilogram",
-            "activity": "electrolysis_of_water"
-        },
-        # PV electricity production
-        {
-            "input": "sunlight",
-            "amount": 1.0,  # !!!! specify the correct amount
-            "type": "biosphere",
-            "unit": "unit",
-            "activity": "pv_electricity_production" 
-        },
-        {
-            "input": "production_pv_panels",
-            "amount": 1.0,  # !!!! specify the correct amount
-            "type": "production",
-            "unit": "kilowatt",  # !!!! specify the correct unit
-            "activity": "pv_electricity_production"
-        },
-        {
-            "input": "electricity",  # produced from PV
-            "amount": 1.0,  # !!!! specify the correct amount
-            "type": "production",
-            "unit": "kilowatt",
-            "activity": "pv_electricity_production"
-        },
-        # excessive power storage
-        {
-            "input": "electricity",  # from PV modules
-            "amount": 1.0,  # !!!! specify the correct amount
-            "type": "technosphere",
-            "unit": "kilowatt",
-            "activity": "excessive_power_storage"
-        },
-        {
-            "input": "production_battery",
-            "amount": 1.0,
-            "type": "production",
-            "unit": "kilowatt",  # !!!! specify the correct unit
-            "activity": "excessive_power_storage"
-        },
-        {
-            "input": "electricity",
-            "amount": 1.0,  # !!!! specify the correct amount
-            "type": "production",
-            "unit": "kilowatt",
-            "activity": "excessive_power_storage"
-        }
-    ]
-}
-#
-# Write the activities to a JSON file
-with open('activities.json', 'w') as file:
-    json.dump(activities, file, indent=4)
-# Write the exchanges to a JSON file
-with open('exchanges.json', 'w') as file:
-    json.dump(exchanges, file, indent=4)
-#
-#loading activities from JSON
-with open('activities.json', 'r') as file:
-    foreground_LCI_database_activities = json.load(file)['activities']
-#loading exchanges from JSON
-with open('exchanges.json', 'r') as file:
-    foreground_LCI_database_exchanges = json.load(file)['exchanges']
-#
-#LCI database, associating the activities with their respective exhanges
-db_name = "foreground_LCI_database"
-LCI_database = {}
-#processing activities
-for activity in foreground_LCI_database_activities:
-    activity_code = activity.pop('code')
-    LCI_database[activity_code] = activity
-#processing exchanges
-for exchange in foreground_LCI_database_exchanges:
-    input_code = exchange.pop('input') 
-    activity_code = exchange.pop('activity')
-    exchange['input'] = (activity_code, input_code)  # this was added to differentiate between the same variables but for different activities, e.g., electricity
-    #exchange['output'] = (input_code, activity_code)
-    LCI_database[activity_code].setdefault('exchanges', []).append(exchange)
-#
-#print(json.dumps(activities, indent=4))
-#print(json.dumps(exchanges, indent=4))
-pp.pprint(LCI_database)
+import json
 
+class Foreground_LCI_Database_Plugin_V1:
+    '''Processing of foreground LCI data for brightway calculation for Photovoltaics Version 1.'''
+    
+    def __init__(self, dcf, print_info):
+        process_table(dcf.inp, 'Irradiation Used', 'Value')
+        process_table(dcf.inp, 'LCA Parameters Photovoltaic', 'Value')
+        
+        self.calculate_sunlight(dcf)
+        self.activities()
+        self.exchanges(dcf, self.calculate_sunlight(dcf))
+        self.LCI_database(self.activities(), self.exchanges(dcf, self.calculate_sunlight(dcf)))
+
+    def calculate_sunlight(self, dcf):
+        '''Calculating the amount of sunlight.'''
+        value = dcf.inp['Irradiation Used']['Data']['Value']
+        if isinstance(value, str):
+            data = read_textfile(value, delimiter='  ')[:, 1]
+        else:
+            data = np.array(value)
+        
+        if len(data.shape) > 1:
+            raise ValueError("Expected 1D array for sunlight data.")
+        
+        total_amount_sunlight = np.sum(data)
+        return total_amount_sunlight
+
+    def activities(self):
+        activities = {
+            "activities": [
+                {
+                    "name": "Pure water production",
+                    "location": "GLO",
+                    "reference product": "pure water",
+                    "unit": "cubic meter",
+                    "code": "pure_water_production"
+                },
+                {
+                    "name": "Electrolysis of water",
+                    "location": "GLO",
+                    "reference product": "hydrogen",
+                    "unit": "kilogram",
+                    "code": "electrolysis_of_water"
+                },
+                {
+                    "name": "PV electricity production",
+                    "location": "GLO/local",
+                    "reference product": "electricity",
+                    "unit": "kilowatt",
+                    "code": "pv_electricity_production"
+                },
+                {
+                    "name": "excessive power storage",
+                    "location": "GLO/local",
+                    "reference product": "electricity",
+                    "unit": "kilowatt",
+                    "code": "excessive_power_storage"
+                }
+            ]
+        }
+        return activities
+
+    def exchanges(self, dcf, total_amount_sunlight):
+        exchanges = {
+            "exchanges": [
+                {
+                    "input": "electricity",
+                    "amount": 1.0,
+                    "type": "technosphere",
+                    "unit": "kilowatt",
+                    "activity": "pure_water_production"
+                },
+                {
+                    "input": "production_reverse_osmosis",
+                    "amount": 1.0,
+                    "type": "production",
+                    "unit": "smt",
+                    "activity": "pure_water_production"
+                },
+                {
+                    "input": "sea water",
+                    "amount": 1.0,
+                    "type": "biosphere",
+                    "unit": "cubic meter",
+                    "activity": "pure_water_production"
+                },
+                {
+                    "input": "pure water",
+                    "amount": 1.0,
+                    "type": "production",
+                    "unit": "cubic meter",
+                    "activity": "pure_water_production"
+                },
+                {
+                    "input": "brine",
+                    "amount": 1.0,
+                    "type": "byproduct",
+                    "unit": "kilogram",
+                    "activity": "pure_water_production"
+                },
+                {
+                    "input": "electricity",
+                    "amount": 1.0,
+                    "type": "technosphere",
+                    "unit": "kilowatt",
+                    "activity": "electrolysis_of_water"
+                },
+                {
+                    "input": "pure water",
+                    "amount": 1.0,
+                    "type": "technosphere",
+                    "unit": "cubic meter",
+                    "activity": "electrolysis_of_water"
+                },
+                {
+                    "input": "production_electrolyzer",
+                    "amount": 1.0,
+                    "type": "technosphere",
+                    "unit": "kilowatt",
+                    "activity": "electrolysis_of_water"
+                },
+                {
+                    "input": "hydrogen",
+                    "amount": 1.0,
+                    "type": "production",
+                    "unit": "kilogram",
+                    "activity": "electrolysis_of_water"
+                },
+                {
+                    "input": "oxygen",
+                    "amount": 1.0,
+                    "type": "byproduct",
+                    "unit": "kilogram",
+                    "activity": "electrolysis_of_water"
+                },
+                {
+                    "input": "sunlight",
+                    "amount": total_amount_sunlight,
+                    "type": "biosphere",
+                    "unit": "unit",
+                    "activity": "pv_electricity_production"
+                },
+                {
+                    "input": "production_pv_panels",
+                    "amount": 1.0,
+                    "type": "production",
+                    "unit": "kilowatt",
+                    "activity": "pv_electricity_production"
+                },
+                {
+                    "input": "electricity",
+                    "amount": 1.0,
+                    "type": "production",
+                    "unit": "kilowatt",
+                    "activity": "pv_electricity_production"
+                },
+                {
+                    "input": "electricity",
+                    "amount": 1.0,
+                    "type": "technosphere",
+                    "unit": "kilowatt",
+                    "activity": "excessive_power_storage"
+                },
+                {
+                    "input": "production_battery",
+                    "amount": 1.0,
+                    "type": "production",
+                    "unit": "kilowatt",
+                    "activity": "excessive_power_storage"
+                },
+                {
+                    "input": "electricity",
+                    "amount": 1.0,
+                    "type": "production",
+                    "unit": "kilowatt",
+                    "activity": "excessive_power_storage"
+                }
+            ]
+        }
+        return exchanges
+
+    def LCI_database(self, activities, exchanges):
+        LCI_database = {}
+        
+        # Process activities
+        for activity in activities['activities']:
+            activity_code = activity.pop('code')
+            LCI_database[activity_code] = activity
+        
+        # Process exchanges
+        for exchange in exchanges['exchanges']:
+            input_code = exchange.pop('input')
+            activity_code = exchange.pop('activity')
+            exchange['input'] = (activity_code, input_code)
+            LCI_database[activity_code].setdefault('exchanges', []).append(exchange)
+        
+        pp.pprint(LCI_database)
