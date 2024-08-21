acc56247aee0f97958430a97b5737b935eceb66b
Kristina Rabatinova <kristina.rabatinova@gmail.com>
2024-08-21 15:04:43 +0200

    updte: adding LCA parameters from Photovoltaic Plugin with dcf.inp function

--- src/pyH2A/Plugins/Foreground_LCI_Database_Plugin_V2_2.py
+++ src/pyH2A/Plugins/Foreground_LCI_Database_Plugin_V2_2.py
@@ -1,4 +1,4 @@
-from pyH2A.Utilities.input_modification import insert, process_table
+from pyH2A.Utilities.input_modification import insert, process_table, read_textfile
 import numpy as np
 import pprint as pp
 import matplotlib.pyplot as plt
@@ -17,11 +17,27 @@ class Foreground_LCI_Database_Plugin_V2_2:
     '''
 
     def __init__(self, dcf, print_info):
+        process_table(dcf.inp, 'Irradiation Used', 'Value')
         process_table(dcf.inp, 'LCA Parameters Photovoltaic', 'Value')
 
-        self.LCI_database(dcf)
+        self.calculate_sunlight(dcf)
+        self.LCI_database(dcf, self.calculate_sunlight(dcf)) 
 
-    def LCI_database(self, dcf):
+    def calculate_sunlight(self, dcf):
+        '''Calculating the amount of sunlight.'''
+        value = dcf.inp['Irradiation Used']['Data']['Value']
+        if isinstance(value, str):
+            data = read_textfile(value, delimiter='  ')[:, 1]
+        else:
+            data = np.array(value)
+        
+        if len(data.shape) > 1:
+            raise ValueError("Expected 1D array for sunlight data.")
+        
+        total_amount_sunlight = np.sum(data)
+        return total_amount_sunlight
+    
+    def LCI_database(self, dcf, total_amount_sunlight):
         #
         activities = {
             "activities": [
@@ -35,7 +51,7 @@ class Foreground_LCI_Database_Plugin_V2_2:
                 {
                     "name": "Production and maintenance of individual parts",
                     "location": "GLO",
-                    "reference product": "hydrogen",
+                    "reference product": "production",
                     "unit": "unit",
                     "code": "production_and_maintenance" 
                 }
@@ -47,65 +63,65 @@ class Foreground_LCI_Database_Plugin_V2_2:
                 #production of hydrogen
                 {
                     "input": "sunlight",
-                    "amount": 1.0,  # !!!! specify the correct amount
+                    "amount": total_amount_sunlight,  
                     "type": "biosphere",
                     "unit": "kilowatt",
                     "activity": "production_of_hydrogen" 
                 },
                 {
                     "input": "sea water",
-                    "amount": dcf.inp['LCA Parameters Photovoltaic']['Sea water demand (m3)']['Value'],  # !!!! specify the correct amount
+                    "amount": dcf.inp['LCA Parameters Photovoltaic']['Sea water demand (m3)']['Value'],  
                     "type": "biosphere",
                     "unit": "cubic meter",
                     "activity": "production_of_hydrogen"
                 }, 
                 {
                     "input": "brine",
-                    "amount": 1.0,  # !!!! specify the correct amount
+                    "amount":  dcf.inp['LCA Parameters Photovoltaic']['Mass of brine (kg)']['Value'],  
                     "type": "byproduct",
                     "unit": "kilogram",
                     "activity": "production_of_hydrogen"
                 },
                 {
-                    "input": "hydrogen",  # for water electrolysis from PV
-                    "amount": 1.0,  # !!!! specify the correct amount
+                    "input": "hydrogen",  
+                    "amount":  dcf.inp['LCA Parameters Photovoltaic']['H2 produced (kg)']['Value'], 
                     "type": "production",
                     "unit": "kilogram",
                     "activity": "production_of_hydrogen"
                 },
                 {
-                    "input": "oxygen",  # for water electrolysis from PV
-                    "amount": 1.0,  # !!!! specify the correct amount
+                    "input": "oxygen",  
+                    "amount":  dcf.inp['LCA Parameters Photovoltaic']['O2 produced (kg)']['Value'],  
                     "type": "byproduct",
                     "unit": "kilogram",
                     "activity": "production_of_hydrogen"
                 },
                 {
                     "input": "production_pv_panels",
-                    "amount": 1.0,  # !!!! specify the correct amount
+                    "amount":  dcf.inp['LCA Parameters Photovoltaic']['Amount of PV modules']['Value'],  
                     "type": "production",
-                    "unit": "kilowatt",  # !!!! specify the correct unit
+                    "unit": "unit",  
                     "activity": "production_and_maintenance" 
                 },
                 {
                     "input": "production_battery",
                     "amount": 1.0,
                     "type": "production",
-                    "unit": "kilowatt",  # !!!! specify the correct unit
+                    "unit": "unit",  
                     "activity": "production_and_maintenance" 
                 },
                 {
                     "input": "production_electrolyzer",
-                    "amount": 1.0,  # if we need to replace this, then here it should change the amount
+                    "amount":  dcf.inp['LCA Parameters Photovoltaic']['Production and maintenance electrolyzer']['Value'],  
                     "type": "technosphere",
-                    "unit": "kilowatt",
+                    "unit": "unit",
                     "activity": "production_and_maintenance" 
                 },
                 {
                     "input": "production_reverse_osmosis",
                     "amount": 1.0,
                     "type": "production",
-                    "unit": "smt",  # !!!! specify the correct unit
+                    "unit": "unit",  # !!!! specify the correct unit
                     "activity": "production_and_maintenance" 
                 }
             ]
@@ -139,8 +155,5 @@ class Foreground_LCI_Database_Plugin_V2_2:
             #exchange['output'] = (input_code, activity_code)
             LCI_database[activity_code].setdefault('exchanges', []).append(exchange)
         #
-        #print(json.dumps(self.activities, indent=4))
-        #print(json.dumps(self.exchanges, indent=4))
-        #print(json.dumps(LCI_database, indent=4))
         #pp.pprint(LCI_database)
         
