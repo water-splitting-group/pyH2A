057bceccd1766ccd89026b364c1074d4e2e26ee7
Abdul Hanan Nawaz <hanannawaz0@gmail.com>
2026-01-06 13:03:20 +0100

    refactor:  I/O spec dictionaries and resolvers for Hourly_Irradiation_Plugin

--- src/pyH2A/Plugins/Hourly_Irradiation_Plugin.py
+++ src/pyH2A/Plugins/Hourly_Irradiation_Plugin.py
@@ -1,7 +1,130 @@
 import numpy as np
 from functools import lru_cache
-
 from pyH2A.Utilities.input_modification import insert, process_table, read_textfile, file_import
+import re
+
+hourly_irradiation_input_dict = {
+    'file': {
+        'top_level': 'Hourly Irradiation',
+        'mid_level': 'File',
+        'lower_level': 'Value',
+    },
+    'module_tilt': {
+        'top_level': 'Irradiance Area Parameters',
+        'mid_level': 'Module Tilt (degrees)',
+        'lower_level': 'Value',
+    },
+    'array_azimuth': {
+        'top_level': 'Irradiance Area Parameters',
+        'mid_level': 'Array Azimuth (degrees)',
+        'lower_level': 'Value',
+    },
+    'nominal_operating_temp': {
+        'top_level': 'Irradiance Area Parameters',
+        'mid_level': 'Nominal Operating Temperature (Celsius)',
+        'lower_level': 'Value',
+    },
+    'mismatch_derating': {
+        'top_level': 'Irradiance Area Parameters',
+        'mid_level': 'Mismatch Derating',
+        'lower_level': 'Value',
+    },
+    'dirt_derating': {
+        'top_level': 'Irradiance Area Parameters',
+        'mid_level': 'Dirt Derating',
+        'lower_level': 'Value',
+    },
+    'temperature_coefficient': {
+        'top_level': 'Irradiance Area Parameters',
+        'mid_level': 'Temperature Coefficient (per Celsius)',
+        'lower_level': 'Value',
+    },
+}
+
+hourly_irradiation_output_dict = {
+    'no_tracking': {
+        'top_level': 'Hourly Irradiation',
+        'mid_level': 'No Tracking (kW)',
+        'lower_level': 'Value',
+    },
+    'single_axis_tracking': {
+        'top_level': 'Hourly Irradiation',
+        'mid_level': 'Horizontal Single Axis Tracking (kW)',
+        'lower_level': 'Value',
+    },
+    'two_axis_tracking': {
+        'top_level': 'Hourly Irradiation',
+        'mid_level': 'Two Axis Tracking (kW)',
+        'lower_level': 'Value',
+    },
+    'mean_no_tracking': {
+        'top_level': 'Hourly Irradiation',
+        'mid_level': 'Mean solar input no tracking (kWh/m2/day)',
+        'lower_level': 'Value',
+    },
+    'mean_single_axis': {
+        'top_level': 'Hourly Irradiation',
+        'mid_level': 'Mean solar input single axis tracking (kWh/m2/day)',
+        'lower_level': 'Value',
+    },
+    'mean_two_axis': {
+        'top_level': 'Hourly Irradiation',
+        'mid_level': 'Mean solar input two axis tracking (kWh/m2/day)',
+        'lower_level': 'Value',
+    },
+    'latitude': {
+        'top_level': 'Hourly Irradiation',
+        'mid_level': 'Latitude',
+        'lower_level': 'Value',
+    },
+    'longitude': {
+        'top_level': 'Hourly Irradiation',
+        'mid_level': 'Longitude',
+        'lower_level': 'Value',
+    },
+}
+
+def resolve_top_levels(inp, pattern):
+    core = pattern.replace('[...]', '').strip()
+    return [k for k in inp if core in k]
+
+def input_resolver(input_dict, dcf):
+    resolved = {}
+    for name, spec in input_dict.items():
+        top_pattern = spec['top_level']
+        mid = spec['mid_level']
+        low = spec['lower_level']
+
+        tops = resolve_top_levels(dcf.inp, top_pattern)
+        collected = {}
+
+        for top in tops:
+            process_table(dcf.inp, top, low)
+            collected[top] = dcf.inp[top][mid][low]
+
+        if len(collected) == 1:
+            resolved[name] = list(collected.values())[0]
+        else:
+            resolved[name] = collected
+    return resolved
+
+def output_resolver(output_dict, values, dcf, print_info=True):
+    for name, spec in output_dict.items():
+        top_pattern = spec['top_level']
+        mid = spec['mid_level']
+        low = spec['lower_level']
+
+        if '[...]' in top_pattern:
+            if isinstance(values[name], dict):
+                for top, val in values[name].items():
+                    insert(dcf, top, mid, low, val, __name__, print_info=print_info)
+            else:
+                for top_key in dcf.inp.keys():
+                    if re.search(top_pattern.replace('[...]', '.*'), top_key):
+                        insert(dcf, top_key, mid, low, values[name], __name__, print_info=print_info)
+        else:
+            insert(dcf, top_pattern, mid, low, values[name], __name__, print_info=print_info)
+
 
 class Hourly_Irradiation_Plugin:
 	'''Calculation of hourly and mean daily irradiation data with different module configurations.
