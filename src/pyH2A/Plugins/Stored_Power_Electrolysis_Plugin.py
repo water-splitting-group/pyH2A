d9d48d1efff5694651f995ad90d80d84d87a1a49
Abdul Hanan Nawaz <hanannawaz0@gmail.com>
2026-01-06 13:54:55 +0100

    refactor:  I/O spec dictionaries for Stored_Power_Electrolysis_Plugin

--- src/pyH2A/Plugins/Stored_Power_Electrolysis_Plugin.py
+++ src/pyH2A/Plugins/Stored_Power_Electrolysis_Plugin.py
@@ -2,6 +2,78 @@ from pyH2A.Utilities.input_modification import insert, process_table, daily_to_y
 from pyH2A.Plugins.Electrolyzer_Plugin import calculate_electrolyzer_power_demand, calculate_hydrogen_production, calculate_stack_replacement
 import numpy as np
 
+stored_power_electrolysis_input_dict = {
+    'fraction_stored_power': {
+        'top_level': 'Electrolysis Using Stored Power',
+        'mid_level': 'Fraction of stored power used for electrolysis',
+        'lower_level': 'Value',
+    },
+    'stored_power_daily': {
+        'top_level': 'Power Generation',
+        'mid_level': 'Stored Power (daily, kWh)',
+        'lower_level': 'Value',
+    },
+    'nominal_power': {
+        'top_level': 'Electrolyzer',
+        'mid_level': 'Nominal Power (kW)',
+        'lower_level': 'Value',
+    },
+    'power_increase_per_year': {
+        'top_level': 'Electrolyzer',
+        'mid_level': 'Power requirement increase per year',
+        'lower_level': 'Value',
+    },
+    'minimum_capacity': {
+        'top_level': 'Electrolyzer',
+        'mid_level': 'Minimum capacity',
+        'lower_level': 'Value',
+    },
+    'conversion_efficiency': {
+        'top_level': 'Electrolyzer',
+        'mid_level': 'Conversion efficiency (kg H2/kWh)',
+        'lower_level': 'Value',
+    },
+    'replacement_time': {
+        'top_level': 'Electrolyzer',
+        'mid_level': 'Replacement time (h)',
+        'lower_level': 'Value',
+    },
+    'yearly_operation_data': {
+        'top_level': 'Electrolyzer',
+        'mid_level': 'Yearly Operation Data',
+        'lower_level': 'Value',
+    },
+    'h2_production_yearly': {
+        'top_level': 'Electrolyzer',
+        'mid_level': 'H2 Production (yearly, kg)',
+        'lower_level': 'Value',
+    },
+}
+
+stored_power_electrolysis_output_dict = {
+    'plant_design_capacity': {
+        'top_level': 'Technical Operating Parameters and Specifications',
+        'mid_level': 'Plant Design Capacity (kg of H2/day)',
+        'lower_level': 'Value',
+    },
+    'electrolyzer_stack_replacement_frequency': {
+        'top_level': 'Planned Replacement',
+        'mid_level': 'Electrolyzer Stack Replacement',
+        'lower_level': 'Frequency (years)',
+    },
+    'stored_power_electrolysis_consumption': {
+        'top_level': 'Power Consumption',
+        'mid_level': 'Stored Power Electrolysis (kWh, yearly)',
+        'lower_level': 'Value',
+    },
+    'stored_power_electrolysis_type': {
+        'top_level': 'Power Consumption',
+        'mid_level': 'Stored Power Electrolysis (kWh, yearly)',
+        'lower_level': 'Type',
+    }
+}
+
+
 class Stored_Power_Electrolysis_Plugin:
     '''Simulation of hydrogen production using electrolysis.
 
