3a41388b762234d6252a22a64adb08f50b963465
Abdul Hanan Nawaz <hanannawaz0@gmail.com>
2026-03-06 08:33:01 +0100

    feat: add input_dict to Stored_Power_Electrolysis_Plugin.

--- src/pyH2A/Plugins/Stored_Power_Electrolysis_Plugin.py
+++ src/pyH2A/Plugins/Stored_Power_Electrolysis_Plugin.py
@@ -2,6 +2,114 @@ from pyH2A.Utilities.input_modification import insert, process_table, daily_to_y
 from pyH2A.Plugins.Electrolyzer_Plugin import calculate_electrolyzer_power_demand, calculate_hydrogen_production, calculate_stack_replacement
 import numpy as np
 
+input_dict = {
+    "Electrolysis Using Stored Power": {
+        "Fraction of stored power used for electrolysis": {
+            "Value": {
+                "type": {float},
+                "bounds": (0, 1) # Fraction or value between 0 and 1. Check Please
+            },
+            "Unit": {
+                "dimension": "dimensionless"
+            },
+            "optional": False,
+            "description": "Fraction of stored power used for electrolysis."
+        }
+    },
+    "Electrolyzer": {
+        "Nominal Power": {
+            "Value": {
+                "type": {float},
+                "bounds": (0, None)
+            },
+            "Unit": {
+                "dimension": "power"
+            },
+            "optional": False,
+            "description": "Nominal power of electrolyzer in kW."
+        },
+        "Power requirement increase per year": {
+            "Value": {
+                "type": {float},
+                "bounds": (0, None)
+            },
+            "Unit": {
+                "dimension": "dimensionless"
+            },
+            "optional": False,
+            "description": "Electrolyzer power requirement increase per year due to stack degradation. Percentage or value > 0. Increase calculated as: (1 + increase per year) ^ year."
+        },
+        "Minimum capacity": {
+            "Value": {
+                "type": {float},
+                "bounds": (0, 1)
+            },
+            "Unit": {
+                "dimension": "dimensionless"
+            },
+            "optional": False,
+            "description": "Minimum capacity required for electrolyzer operation. Percentage or value between 0 and 1."
+        },
+        "Conversion efficiency": {
+            "Value": {
+                "type": {float},
+                "bounds": (0, None)
+            },
+            "Unit": {
+                "dimension": "mass / energy"
+            },
+            "optional": False,
+            "description": "Electrical conversion efficiency of electrolyzer in (kg H2)/kWh."
+        },
+        "Replacement time": {
+            "Value": {
+                "type": {float},
+                "bounds": (0, None)
+            },
+            "Unit": {
+                "dimension": "time"
+            },
+            "optional": False,
+            "description": "Operating time in hours before stack replacement of electrolyzer is required."
+        },
+        "Yearly Operation Data": {
+            "Value": {
+                "type": {np.ndarray},
+                "bounds": (0, None)
+            },
+            "Unit": {
+                "dimension": "(year, H2 produced, electrolyzer capacity)"
+            },
+            "optional": False,
+            "description": "Yearly operation data of electrolyzer in (year, H2 produced, electrolyzer capacity) format."
+        },
+        "H2 Production (yearly)": {
+            "Value": {
+                "type": {np.ndarray},
+                "bounds": (0, None)
+            },
+            "Unit": {
+                "dimension": "mass"
+            },
+            "optional": False,
+            "description": "Yearly hydrogen production in kg."
+        },
+    },
+    "Power Generation": {
+        "Stored Power (daily)": {
+            "Value": {
+                "type": {dict,},
+                "bounds": (0, None)
+            },
+            "Unit": {
+                "dimension": "energy"
+            },
+            "optional": False,
+            "description": "Power stored in battery daily in kWh (dictionary of years)."
+        }
+    },
+}
+
 class Stored_Power_Electrolysis_Plugin:
     '''Simulation of hydrogen production using electrolysis.
 
