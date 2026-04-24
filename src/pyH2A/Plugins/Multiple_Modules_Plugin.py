7a8a3540ce268a0e6a8a0c0a4baab94f4f38e9ce
axfache <axel.fache@uni-jena.de>
2026-04-24 06:56:54 +0200

    Multiple_Modules_Plugin correct capitalization

--- src/pyH2A/Plugins/Multiple_Modules_Plugin.py
+++ src/pyH2A/Plugins/Multiple_Modules_Plugin.py
@@ -115,9 +115,9 @@ class Multiple_Modules_Plugin:
 		'''Calculation of total required staff for all plant modules, then scaling down to staff
 		requirements for one module.'''
 
-		area = self.input_dict_resolved['Technical Operating Parameters and Specifications']['Plant Modules']['Value'].unit['-'] * self.input_dict_resolved['Non-Depreciable Capital Costs']['Solar Collection Area']['Value'].unit['m2']
+		area = self.input_dict_resolved['Technical Operating Parameters and Specifications']['Plant modules']['Value'].unit['-'] * self.input_dict_resolved['Non-Depreciable Capital Costs']['Solar collection area']['Value'].unit['m2']
 
 		staff = np.ceil(area / self.input_dict_resolved['Fixed Operating Costs']['Solar collection area per staffer']['Value']).unit['m2'] + self.input_dict_resolved['Fixed Operating Costs']['Number of supervisors']['Value'].unit['-']
 		staff = staff * self.input_dict_resolved['Fixed Operating Costs']['Number of 8-hour shifts']['Value'].unit['-']
 
-		self.staff_per_module = Quantity(staff / self.input_dict_resolved['Technical Operating Parameters and Specifications']['Plant Modules']['Value'].unit['-'], '-')
+		self.staff_per_module = Quantity(staff / self.input_dict_resolved['Technical Operating Parameters and Specifications']['Plant modules']['Value'].unit['-'], '-')
