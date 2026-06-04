d13824b9b5c6a41bb91452a050c983f5f52794f1
Jacob Schneidewind <41544471+jschneidewind@users.noreply.github.com>
2026-06-04 15:50:24 +0200

    Update of units, fixed "liters" in input_resolver_test_data

--- src/pyH2A/Utilities/Unit_Handler/config.py
+++ src/pyH2A/Utilities/Unit_Handler/config.py
@@ -3,6 +3,8 @@ Configuration for the custom pyH2A lightweight unit handler.
 Defines supported dimensions, base units, and conversions.
 """
 
+from scipy import constants as con
+
 # Temperature handles as a special case because of offsets vs multipliers
 ABSOLUTE_TEMPERATURE = {
     "base": "K",
@@ -30,9 +32,11 @@ DIMENSIONS = {
             "kWh": 3.6e6,
             "MWh": 3.6e9,
             "GWh": 3.6e12,
+            "TWh": 3.6e15,
             "eV": 1.602176634e-19,
             "cal": 4.184,
-            "kcal": 4184.0
+            "kcal": 4184.0, 
+            "toe": 4.1868e10
         }
     },
     "power": {
@@ -42,7 +46,25 @@ DIMENSIONS = {
             "kW": 1e3,
             "MW": 1e6,
             "GW": 1e9,
-            "hp": 745.699872
+            "hp": 745.699872, # imperial horsepower 
+            "cv": 735.49875,  # metric horsepower
+            "J_per_day": 1./86400.,
+            "kJ_per_day": 1e3/86400.,
+            "MJ_per_day": 1e6/86400.,
+            "GJ_per_day": 1e9/86400.,
+            "Wh_per_day": 1./24.,
+            "kWh_per_day": 1e3/24.,
+            "MWh_per_day": 1e6/24.,            
+            "GWh_per_day": 1e9/24.,   
+            "J_per_year": 1./(86400.*365.),
+            "kJ_per_year": 1e3/(86400.*365.),
+            "MJ_per_year": 1e6/(86400.*365.),
+            "GJ_per_year": 1e9/(86400.*365.),            
+            "Wh_per_year": 1./(24.*365.),                     
+            "kWh_per_year": 1e3/(24.*365.), 
+            "MWh_per_year": 1e6/(24.*365.), 
+            "GWh_per_year": 1e9/(24.*365.),             
+            "TWh_per_year": 1e12/(24.*365.),             
         }
     },
     "length": {
@@ -51,7 +73,9 @@ DIMENSIONS = {
             "m": 1.0,
             "mm": 1e-3,
             "cm": 1e-2,
-            "km": 1e3
+            "km": 1e3, 
+            "ft": 0.3048, 
+            "in": 0.0254
         }
     },
     "area": {
@@ -71,23 +95,23 @@ DIMENSIONS = {
             "m3": 1.0,
             "mm3": 1e-9,
             "cm3": 1e-6,
-            "L": 1e-3,
-            "liters": 1e-3,
             "km3": 1e9,
+            "uL": 1e-9,
             "mL": 1e-6,
-            "uL": 1e-9
+            "L": 1e-3,
+            "liter": 1e-3,
         }
     },
     "time": {
         "base": "s",
         "conversions": {
             "s": 1.0,
+            "ms": 1e-3,
             "minute": 60.0,
             "h": 3600.0,
             "day": 86400.0,
+            "week": 604800.0,
             "year": 31536000.0, # Assuming 365 days in a year for simplicity
-            "ms": 1e-3,
-            "week": 604800.0
         }
     },
     "currency": {
@@ -118,7 +142,11 @@ DIMENSIONS = {
         "conversions": {
             "mol": 1.0,
             "umol": 1e-6,
-            "mmol": 1e-3
+            "mmol": 1e-3, 
+            "kmol": 1e3,
+            "entity": 1/con.Avogadro, 
+            "Nm3": 44.6150334063, # reference 0°C, 1 atm
+            "Sm3":  42.2925433799 # reference 15°C, 1 atm
         }
     },
     "voltage": {
@@ -146,9 +174,9 @@ DIMENSIONS = {
         "conversions": {
             "Pa": 1.0,
             "hPa": 100.0,
+            "MPa": 1e6,
             "atm": 101325.0,
             "bar": 1e5,
-            "MPa": 1e6,
             "psi": 6894.757293168
         }
     },
@@ -173,8 +201,10 @@ DIMENSIONS = {
         "conversions": {
             "C": 1.0,
             "mC": 1e-3,
+            "F": 9.64853321233100184e4, # Faraday constant
             "Ah": 3600.0,
-            "mAh": 3.6
+            "mAh": 3.6, 
+            "e": 1.602176634e-19
         }
     },
     "resistance": {
