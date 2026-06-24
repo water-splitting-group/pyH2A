786eb6afc258729b7c9cc78853972803a2cc4a04
Abdul Hanan Nawaz <hanannawaz0@gmail.com>
2026-06-24 21:44:36 +0200

    bug: update mol_H2_per_m2_per_day in PEC plugin.

--- src/tests/plugins/pec_plugin_test.py
+++ src/tests/plugins/pec_plugin_test.py
@@ -94,7 +94,7 @@ class DummyDCF:
                 "total_solar_collection_area": Quantity(47057.399999999994, 'm2'),
                 "cell_cost": Quantity(988205399.9999998, 'USD'),
                 "cell_number": Quantity(26143.0, '-'),
-                "mol_H2_per_m2_per_day": Quantity(10.6256954979, 'mol/day/m2'),
+                "mol_H2_per_m2_per_day": Quantity(10.625699702590028, 'mol/day/m2'),
                 "flowrate_H2_per_cell": Quantity(0.0382525189293241, 'kg/day'),
                 "total_land_area": Quantity(5348817.431990256, 'm2'),
             },
