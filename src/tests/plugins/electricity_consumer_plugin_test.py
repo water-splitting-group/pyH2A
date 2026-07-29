6fa1bda74d8f9a16ab3391106799d5309851f106
axfache <axel.fache@uni-jena.de>
2026-07-29 11:12:17 +0200

    Create Electricity_Consumer_Plugin Also create csv file for consumption, plugin test, e2e PV_w_consumer_Base_test for illustration purposes, and update documentation

---
+++ src/tests/plugins/electricity_consumer_plugin_test.py
@@ -0,0 +1,89 @@
+import pytest
+import numpy as np
+from pyH2A.Utilities.Unit_Handler.quantity import Quantity
+from pyH2A.Plugins.Electricity_Consumer_Plugin import (
+    Electricity_Consumer_Plugin
+)
+
+
+class DummyDCF:
+    """DCF object for Electricity_Consumer_Plugin with configurable inputs."""
+
+    def __init__(
+        self,
+        operation_years_relative,
+        hourly_file,
+        available_energy_hourly
+    ):
+        self.inp = {
+            "Time": {
+                "Years": {
+                    "Value": operation_years_relative,
+                    "Unit": "-",   
+                    "Processed": "Yes",                    
+                },
+            },            
+            "Hourly Consumer Profile": {"File": {"Value": hourly_file}},
+            "Power Generation": {
+                "Available energy (hourly)": {"Value": available_energy_hourly, "Unit" : "kWh"},
+            },           
+        }
+
+
+@pytest.mark.parametrize(
+    "case",
+    [
+        {
+            "input": {
+                "operation_years_relative": {'Operation years relative': np.arange(0, 2)},       
+                "hourly_file": "pyH2A.Lookup_Tables.Hourly_Consumption~Constant_consumption_10MW.csv ",
+                "available_energy_hourly": {0: 2*np.arange(0, 8760), 1:2*np.arange(0, 8760)},
+            },
+            "expected": {
+                "first_6h_default_energy":Quantity(np.array([
+                    1e4, 9998, 9996, 
+                    9994, 9992, 9990
+                    ]), 'kWh'),
+                "last_6h_available_energy":Quantity(np.array([
+                    7508, 7510, 7512, 
+                    7514, 7516, 7518
+                    ]), 'kWh'),
+                "last_6h_default_energy":Quantity(np.zeros(6), 'kWh'),
+                "first_6h_available_energy":Quantity(np.zeros(6), 'kWh'),                    
+            },
+        },
+    ],
+)
+def test_electricity_consumer_plugin(case):
+    """Test Electricity_Consumer_Plugin."""
+
+    # Unpack inputs from case
+    dcf = DummyDCF(**case["input"])
+
+    # Run plugin
+    plugin = Electricity_Consumer_Plugin(dcf, print_info=False)
+    expected = case["expected"]
+    
+    # Tolerance (very small)
+    tolerance = 1e-12
+
+    assert plugin.unsatisfied_demand[0].unit["J"][0:6] == pytest.approx(
+        expected["first_6h_default_energy"].unit["J"],
+        abs=tolerance
+    )
+
+    assert plugin.unsatisfied_demand[1].unit["J"][-6:] == pytest.approx(
+        expected["last_6h_default_energy"].unit["J"],
+        abs=tolerance
+    )
+
+    assert plugin.total_electric_energy_available_yearly_data[0].unit["J"][0:6] == pytest.approx(
+        expected["first_6h_available_energy"].unit["J"],
+        abs=tolerance
+    )
+
+    assert plugin.total_electric_energy_available_yearly_data[1].unit["J"][-6:] == pytest.approx(
+        expected["last_6h_available_energy"].unit["J"],
+        abs=tolerance
+    )    
+
