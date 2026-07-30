4f5a90dcb7082ac0fb285115327cba3bea2649a4
Abdul Hanan Nawaz <hanannawaz0@gmail.com>
2026-07-30 19:37:57 +0200

    bug: upadte the broken path

--- src/tests/e2e_lcoh/pec_lcoh_test.py
+++ src/tests/e2e_lcoh/pec_lcoh_test.py
@@ -7,7 +7,7 @@ from pyH2A.Utilities.Unit_Handler import Quantity
     [
         {
             "input": {
-                "input_file": "src/tests/end_to_end/PEC/PEC_Base_test.md",
+                "input_file": "src/tests/end_to_end/PEC_Base_test.md",
                 "output_directory": "src/tests/end_to_end/",
             },
             "expected": {
